0901fb85add10b72ee4701bdaec34ff1a7a3e71f
Dekkia <github@dekkia.de>
2021-04-16 17:23:10 +0200

    updated logging-script to support latest speedtest-tool

--- logger.sh
+++ logger.sh
@@ -6,20 +6,16 @@ dbUser="userName"
 dbPassword="-pPassword" #Add -p in to the beginning of your password.
 dbName="internetspeed"
 
-res=$(speedtest-cli --simple)
+res=$(speedtest --format=json --unit=Mibps)
 
-res1=$(echo "$res" | sed -n '1p'| cut -d" " -f2)
-res2=$(echo "$res" | sed -n '2p'| cut -d" " -f2)
-res3=$(echo "$res" | sed -n '3p'| cut -d" " -f2)
+res1=$(echo "$res" | jq '.ping.latency')
+res2=$(expr $(echo "$res" | jq '.upload.bandwidth') / 131072)
+res3=$(expr $(echo "$res" | jq '.download.bandwidth') / 131072)
 
 em=" "
 
 date=$(date "+%Y-%m-%d")$em$(date "+%H:%M:%S")
 #echo "$date"
-
-#endRes=$date$res1$d$res2$d$res3
-
-#echo "$endRes" >> speedLog/logFile.csv
 
 mysql -h $dbHost  -u $dbUser $dbPassword $dbName << EOF
 INSERT INTO internetspeed.log (ID, measureTime, ping, up, down) VALUES (NULL, '$date', '$res1', '$res2', '$res3');
